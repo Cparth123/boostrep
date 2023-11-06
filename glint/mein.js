a3ca776c92885b14ee0fa2fa906313264ecd16da
Cparth123 <126954051+Cparth123@users.noreply.github.com>
2023-11-06 13:49:39 +0530

    Add files via upload

---
+++ glint/mein.js
@@ -0,0 +1,42 @@
+
+// menu_slider event
+let s2 = document.getElementById("s2");
+let aero = document.getElementById("aero");
+aero.addEventListener("click", pc);
+function pc(){
+    document.getElementById("myAnchor").href = "#s2";
+}
+
+
+
+// // number_counting
+// let num = document.querySelectorAll("#num");
+// console.log(num[0] = 0);
+
+// comments-slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
