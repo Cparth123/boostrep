4b201a170823d8815e964cc6c7d8e1f74e11828e
Cparth123 <126954051+Cparth123@users.noreply.github.com>
2023-11-06 13:52:26 +0530

    Add files via upload

---
+++ salaad/script.js
@@ -0,0 +1,62 @@
+VanillaTilt.init(document.querySelector(".container"), {
+  max: 2,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2
+  
+});
+
+
+// sec2
+VanillaTilt.init(document.querySelector(".conten3text"), {
+  max: 5,
+  speed: 900,
+  glare: true,
+  "max-glare": 0.2
+});
+
+// sec3 cards
+VanillaTilt.init(document.querySelector(".card1"), {
+  max: 5,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2
+});
+
+VanillaTilt.init(document.querySelector(".card2"), {
+  max: 5,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2
+});
+VanillaTilt.init(document.querySelector(".card3"), {
+  max: 5,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2
+});
+
+
+
+// sec4
+VanillaTilt.init(document.querySelector(".conten5text"), {
+  max: 5,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2
+});
+
+
+// sec6
+VanillaTilt.init(document.querySelector(".conten7text"), {
+  max: 5,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2
+});
+
+
+
+
+
+// Built this calculator following this guide: https://www.youtube.com/watch?v=NhcZh8Bwr30&t=5s
